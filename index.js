048ca2f7138aa702862f971e2aa39059ff93ae4d
Hammam AbuShehadeh <hammam.y.asmar@gmail.com>
2023-09-25 02:40:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+// function multiplied(num1,num2){
+//     let multiplied = num1 * num2
+//     return multiplied === 62 ? "results will equal 62" : "results will NOT equal 62"
+// }
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let num3 = 28;
+let num4 = 8;
+let mod = num3 % num4;
+let max = Math.max(0, 1, 5, 10, 15, 20, 18, 3);
